65ac3b889772fb782f55a7cf2bb503bc0eb78933
Stanislav Ochotnicky <sochotnicky@gmail.com>
2010-05-03 00:07:47 +0200

    Moved stuff around, created installer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='amt',
+      version='1.0',
+      description='Module to simplify access to movies on apple.com/trailers',
+      author='Stanislav Ochotnicky',
+      author_email='sochotnicky@gmail.com',
+      url='http://www.python.org/',
+      packages=['amt'],
+     )
